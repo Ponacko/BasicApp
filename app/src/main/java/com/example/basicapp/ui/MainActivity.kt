96bea7dc4b209c8801939c3cab1b4c6c0ffb79a6
Ponacko <ponacko@gmail.com>
2024-09-14 15:39:33 +0200

    get user data from the UI and save/load it

--- app/src/main/java/com/example/basicapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/basicapp/ui/MainActivity.kt
@@ -4,7 +4,9 @@ import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,31 +17,62 @@ class MainActivity : ComponentActivity() {
 
     private val viewModel: MainViewModel by viewModel()
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var fetchButton: Button
+    private lateinit var saveButton: Button
+    private lateinit var loadButton: Button
+    private lateinit var firstNameEditText: EditText
+    private lateinit var lastNameEditText: EditText
+    private lateinit var loadedUserText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        val fetchButton = findViewById<Button>(R.id.fetchButton)
+        findViews()
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = ItemAdapter(emptyList())
 
-        fetchButton.setOnClickListener {
-            progressBar.visibility = View.VISIBLE
-            recyclerView.visibility = View.GONE
-            viewModel.fetchItems()
-        }
+        setOnClickListeners()
+        setObservers()
+    }
+
+    private fun findViews() {
+        recyclerView = findViewById(R.id.recyclerView)
+        progressBar = findViewById(R.id.progressBar)
+        fetchButton = findViewById(R.id.fetchButton)
+        saveButton = findViewById(R.id.saveButton)
+        loadButton = findViewById(R.id.loadButton)
+        firstNameEditText = findViewById(R.id.firstName)
+        lastNameEditText = findViewById(R.id.lastName)
+        loadedUserText = findViewById(R.id.loadedUserText)
+    }
+
+    private fun setObservers() {
         viewModel.items.observe(this) { items ->
-            progressBar.visibility = View.GONE
-            recyclerView.visibility = View.VISIBLE
+            displayList()
             (recyclerView.adapter as ItemAdapter).submitList(items.toMutableList())
         }
         viewModel.error.observe(this) { errorMessage ->
-            progressBar.visibility = View.GONE
-            recyclerView.visibility = View.VISIBLE
+            displayList()
             showErrorDialog(errorMessage)
+        }
+    }
+
+    private fun setOnClickListeners() {
+        fetchButton.setOnClickListener {
+            displayLoading()
+            viewModel.fetchItems()
+        }
+        saveButton.setOnClickListener {
+            val user = viewModel.createUserFromInput(firstNameEditText.text.toString(),
+                lastNameEditText.text.toString())
+            viewModel.saveUser(user)
+        }
+
+        loadButton.setOnClickListener {
+            viewModel.loadUser(1)
         }
     }
 
@@ -52,5 +85,15 @@ class MainActivity : ComponentActivity() {
             }
             .setCancelable(false)
             .show()
+    }
+
+    private fun displayList() {
+        progressBar.visibility = View.GONE
+        recyclerView.visibility = View.VISIBLE
+    }
+
+    private fun displayLoading() {
+        progressBar.visibility = View.VISIBLE
+        recyclerView.visibility = View.GONE
     }
 }
