fe2116c9db7e1ae656bd631b5cae96e86481d4cc
Ponacko <ponacko@gmail.com>
2024-09-14 20:51:19 +0200

    implement getting Items from DB and saving the ones from API

--- app/src/main/java/com/example/basicapp/repository/ItemRepository.kt
+++ app/src/main/java/com/example/basicapp/repository/ItemRepository.kt
@@ -1,13 +1,27 @@
 package com.example.basicapp.repository
 
+import com.example.basicapp.data.ItemDao
+import com.example.basicapp.model.Item
+import com.example.basicapp.model.ItemMapper
 import com.example.basicapp.network.ItemNetworkSource
 
 class ItemRepository(
-    private val itemNetworkSource: ItemNetworkSource
+    private val itemNetworkSource: ItemNetworkSource,
+    private val itemDao: ItemDao
 ) {
-    fun getItemsFromDatabase() {
-        // TODO implement
+    suspend fun getItemsFromDatabase(): List<Item> {
+        val itemEntities = itemDao.getAll()
+        val items = ItemMapper.mapToModelList(itemEntities)
+        return items
     }
 
-    suspend fun getItemsFromApi() = itemNetworkSource.fetchItems()
+    suspend fun getItemsFromApi(): List<Item> {
+        val items = itemNetworkSource.fetchItems()
+        if (items.isNotEmpty()) {
+            itemDao.deleteAll()
+            val itemEntities = ItemMapper.mapToEntityList(items)
+            itemDao.insertAll(itemEntities)
+        }
+        return items
+    }
 }
