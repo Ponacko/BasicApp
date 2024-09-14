5ec93507899d09bd4bc6119ecd3955ec22f78fd3
Ponacko <ponacko@gmail.com>
2024-09-14 13:10:56 +0200

    add error handling via displayed dialog

--- app/src/main/java/com/example/basicapp/MainViewModel.kt
+++ app/src/main/java/com/example/basicapp/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.basicapp
 
+import android.net.http.HttpException
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,6 +8,8 @@ import androidx.lifecycle.viewModelScope
 import com.example.basicapp.model.Item
 import com.example.basicapp.repository.ItemRepository
 import kotlinx.coroutines.launch
+import org.json.JSONException
+import java.io.IOException
 
 class MainViewModel(
     private val itemRepository: ItemRepository
@@ -15,10 +18,21 @@ class MainViewModel(
     private val _items = MutableLiveData<List<Item>>()
     val items: LiveData<List<Item>>
         get() = _items
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> get() = _error
 
     fun fetchItems() {
         viewModelScope.launch {
-            _items.value = itemRepository.getItemsFromApi()
+            try {
+                val itemsFromApi = itemRepository.getItemsFromApi()
+                _items.value = itemsFromApi
+            } catch (e: IOException) {
+                _error.value = "Network error. Please check your internet connection. ${e.message}"
+            } catch (e: JSONException) {
+                _error.value = "Data error. Unable to process the response. ${e.message}"
+            } catch (e: Exception) {
+                _error.value = "An unexpected error occurred: ${e.message}"
+            }
         }
     }
 }
--- app/src/main/java/com/example/basicapp/network/FakeItemNetworkSource.kt
+++ app/src/main/java/com/example/basicapp/network/FakeItemNetworkSource.kt
@@ -4,23 +4,25 @@ import com.example.basicapp.model.Item
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
+import java.io.IOException
 import kotlin.random.Random
 
 class FakeItemNetworkSource: ItemNetworkSource {
 
-    // Simulate a network delay
     private val delayTimeMillis = 2000L // 2 seconds
 
-    // returns random number of fake items
     override suspend fun fetchItems(): List<Item> = withContext(Dispatchers.IO) {
         // Simulate network delay
         delay(delayTimeMillis)
 
-        // Generate fake items
+        return@withContext generateRandomAmountOfItems()
+    }
+
+    private fun generateRandomAmountOfItems(): List<Item> {
         val randomCount = Random.nextInt(1, 20)
         val items = List(randomCount) { i ->
             Item(i, "Fake item $i")
         }
-        items
+        return items
     }
 }
