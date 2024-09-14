05871d4418b88641fa243388621ee835c61021de
Ponacko <ponacko@gmail.com>
2024-09-14 20:50:56 +0200

    add ItemMapper

---
+++ app/src/main/java/com/example/basicapp/model/ItemMapper.kt
@@ -0,0 +1,29 @@
+package com.example.basicapp.model
+
+import com.example.basicapp.data.ItemEntity
+
+// Define a mapper object
+object ItemMapper {
+
+    fun mapToEntity(item: Item): ItemEntity {
+        return ItemEntity(
+            id = item.id,
+            name = item.name
+        )
+    }
+
+    fun mapToModel(itemEntity: ItemEntity): Item {
+        return Item(
+            id = itemEntity.id,
+            name = itemEntity.name
+        )
+    }
+
+    fun mapToEntityList(items: List<Item>): List<ItemEntity> {
+        return items.map { mapToEntity(it) }
+    }
+
+    fun mapToModelList(itemEntities: List<ItemEntity>): List<Item> {
+        return itemEntities.map { mapToModel(it) }
+    }
+}
