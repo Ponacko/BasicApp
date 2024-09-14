dcea792c062f6055c9af91f0d1ffa7a986e2276d
Ponacko <ponacko@gmail.com>
2024-09-14 11:27:48 +0200

    add FakeItemNetworkSource

---
+++ app/src/main/java/com/example/basicapp/network/FakeItemNetworkSource.kt
@@ -0,0 +1,10 @@
+package com.example.basicapp.network
+
+import com.example.basicapp.model.Item
+
+class FakeItemNetworkSource: ItemNetworkSource {
+    override fun fetchItems(): List<Item> {
+        TODO("Not yet implemented")
+    }
+
+}
