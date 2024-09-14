f1061427f5a67c2bd6f52f5b9a39111617b5b91b
Ponacko <ponacko@gmail.com>
2024-09-14 12:28:21 +0200

    add ItemAdapter

---
+++ app/src/main/java/com/example/basicapp/ItemAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.basicapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.basicapp.model.Item
+
+class ItemAdapter(
+    private var itemList: MutableList<Item>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView: TextView = itemView.findViewById(R.id.itemTitle)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+        return ItemViewHolder(view)
+    }
+
+    override fun getItemCount() = itemList.size
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val currentItem = itemList[position]
+        holder.textView.text = currentItem.name
+    }
+
+    fun submitList(newItemList: MutableList<Item>) {
+        this.itemList = newItemList
+        notifyDataSetChanged()
+    }
+}
