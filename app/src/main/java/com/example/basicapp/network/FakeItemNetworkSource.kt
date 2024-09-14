f52c5b4a4ce38ca3fe3bc34389d152d84b8e65e0
Ponacko <ponacko@gmail.com>
2024-09-14 12:29:01 +0200

    add FakeItemNetworkSource

--- app/src/main/java/com/example/basicapp/network/FakeItemNetworkSource.kt
+++ app/src/main/java/com/example/basicapp/network/FakeItemNetworkSource.kt
@@ -1,10 +1,16 @@
 package com.example.basicapp.network
 
 import com.example.basicapp.model.Item
+import kotlin.random.Random
 
 class FakeItemNetworkSource: ItemNetworkSource {
-    override fun fetchItems(): List<Item> {
-        TODO("Not yet implemented")
-    }
 
+    // returns random number of fake items
+    override fun fetchItems(): List<Item> {
+        val randomCount = Random.nextInt(1, 20)
+        val items = List(randomCount) { i ->
+            Item(i, "Fake item $i")
+        }
+        return items
+    }
 }
