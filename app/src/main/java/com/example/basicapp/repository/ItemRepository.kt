685ea55b8f56a0eb3598a8d70c552346a19d3578
Ponacko <ponacko@gmail.com>
2024-09-14 21:04:10 +0200

    put ItemRepository operations to IO thread

--- app/src/main/java/com/example/basicapp/repository/ItemRepository.kt
+++ app/src/main/java/com/example/basicapp/repository/ItemRepository.kt
@@ -4,24 +4,26 @@ import com.example.basicapp.data.ItemDao
 import com.example.basicapp.model.Item
 import com.example.basicapp.model.ItemMapper
 import com.example.basicapp.network.ItemNetworkSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class ItemRepository(
     private val itemNetworkSource: ItemNetworkSource,
     private val itemDao: ItemDao
 ) {
-    suspend fun getItemsFromDatabase(): List<Item> {
+    suspend fun getItemsFromDatabase(): List<Item> = withContext(Dispatchers.IO) {
         val itemEntities = itemDao.getAll()
         val items = ItemMapper.mapToModelList(itemEntities)
-        return items
+        return@withContext items
     }
 
-    suspend fun getItemsFromApi(): List<Item> {
+    suspend fun getItemsFromApi(): List<Item> = withContext(Dispatchers.IO) {
         val items = itemNetworkSource.fetchItems()
         if (items.isNotEmpty()) {
             itemDao.deleteAll()
             val itemEntities = ItemMapper.mapToEntityList(items)
             itemDao.insertAll(itemEntities)
         }
-        return items
+        return@withContext items
     }
 }
